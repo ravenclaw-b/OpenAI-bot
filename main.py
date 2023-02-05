ea4e48f67e56b4153764f44b0c68239367b40f9e
bamdad <bamdad.kho@gmail.com>
2023-02-05 14:32:49 +0200

    imported open ai and added ability to prompt it

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ load_dotenv()
 
 KEY = os.getenv("KEY")
 
-print(KEY)
+openai.api_key = KEY
+
+prompt=""
+
+response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=50)
+
+print(response["choices"][0]["text"])
